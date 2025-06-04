6446f542ce0eccd2bcc01219afde6ae3e09acb9c
dpsena <dpsena210@gmail.com>
2025-06-04 11:05:06 -0300

    mockito for controller

--- src/test/java/com/infuse/consulta_creditos_backend/ConsultaControllerTest.java
+++ src/test/java/com/infuse/consulta_creditos_backend/ConsultaControllerTest.java
@@ -6,7 +6,6 @@ import com.infuse.consulta_creditos_backend.services.CreditoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
